0bf1dca018dbfcb2055db23ba7625b61d9a2a92e
Ednilson Martins <juniorzmartin@gmail.com>
2022-07-20 09:06:09 -0300

    Carregando imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,6 +1,7 @@
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 
@@ -24,8 +25,17 @@ export class ProdutosPage {
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response =>{
         this.items = response['content'];
+        this.loadImageUrls();
       },error => {});
+  }
 
-
+  loadImageUrls(){
+    for(var i = 0; i < this.items.length; i++){
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response =>{
+          item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`;
+        }, error =>{})
+    }
   }
 }
