85d66726bfbe324eea42c8256c5094b7a87f6ae0
Ednilson Martins <juniorzmartin@gmail.com>
2022-07-21 11:23:34 -0300

    Criando pagina de carrinho de compras

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { of } from "rxjs/observable/of";
 import { STORAGE_KEYS } from "../config/storage_keys.config";
+import { Cart } from "../models/cart";
 import { LocalUser } from "../models/local_user";
 
 @Injectable()
@@ -20,6 +21,24 @@ export class StorageService {
             localStorage.removeItem(STORAGE_KEYS.localUser);
         }else{
             localStorage.setItem(STORAGE_KEYS.localUser, JSON.stringify(obj));
+        }
+    }
+
+
+    getCart() : Cart {
+        let str = localStorage.getItem(STORAGE_KEYS.cart);
+        if(str != null){
+            return JSON.parse(str);
+        }else{
+            return null;
+        }
+    }
+
+    setCart(obj: Cart){
+        if(obj != null){
+            localStorage.setItem(STORAGE_KEYS.cart, JSON.stringify(obj));
+        }else{
+            localStorage.removeItem(STORAGE_KEYS.cart);
         }
     }
 
