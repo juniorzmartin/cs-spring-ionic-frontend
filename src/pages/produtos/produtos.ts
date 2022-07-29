466fa7c7c242a52ee134f1519526d75b739eaa13
Ednilson Martins <juniorzmartin@gmail.com>
2022-07-29 09:31:06 -0300

    Infinite scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -12,7 +12,10 @@ import { ProdutoService } from '../../services/domain/produto.service';
 })
 export class ProdutosPage {
 
-  items : ProdutoDTO[];
+  items : ProdutoDTO[] = [];
+  page : number = 0;
+  checkComplete: boolean = false;
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
@@ -27,17 +30,23 @@ export class ProdutosPage {
   loadData(){
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoria_id)
+    this.produtoService.findByCategoria(categoria_id, this.page, 10)
       .subscribe(response =>{
-        this.items = response['content'];
+        let start = this.items.length;
+        this.items = this.items.concat(response['content']);
+        let end = this.items.length - 1;
         loader.dismiss();
-        this.loadImageUrls();
+        console.log(this.page);
+        this.loadImageUrls(start, end);
+        if(response["last"] == true) {
+          this.checkComplete = true;
+        }
       },error => {
         loader.dismiss();
       });
   }
-  loadImageUrls(){
-    for(var i = 0; i < this.items.length; i++){
+  loadImageUrls(start: number , end: number){
+    for(var i = start; i <= end; i++){
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response =>{
@@ -58,9 +67,19 @@ export class ProdutosPage {
   }  
 
   doRefresh(event) {
+    this.page = 0;
+    this.items = [];
+    this.checkComplete = false;
     this.loadData();
     setTimeout(() => {
       event.complete();
     }, 1000);
+  }
+  doInfinite(infiniteScroll){
+    this.page++;
+    this.loadData();
+    setTimeout(()=>{
+      infiniteScroll.complete();
+    },1000);
   }
 }
